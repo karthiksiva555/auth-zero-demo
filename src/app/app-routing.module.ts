795f8317ede5708009bae8b5b0b9c4c88df7f82d
Siva Narisetty <sivanarisetty@geotab.com>
2022-12-06 09:32:35 -0500

    Initial commit - Auth Organizations demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '@auth0/auth0-angular';
+import { HomeComponent } from './home/home.component';
+import { LoginButtonComponent } from './login-button/login-button.component';
+import { ProtectedComponent } from './protected/protected.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo:'/home', pathMatch: 'full'},
+  { path: 'login', component: LoginButtonComponent},
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
+  { path: 'protected', component: ProtectedComponent, canActivate: [AuthGuard]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AuthModule } from '@auth0/auth0-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginButtonComponent } from './login-button/login-button.component';
+import { ProtectedComponent } from './protected/protected.component';
+import { HomeComponent } from './home/home.component';
+import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginButtonComponent,
+    ProtectedComponent,
+    HomeComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      // domain: 'dev-hf8bc21m.us.auth0.com',
+      // clientId: 'KKOl3JioGn85QMBVm18NIHQNtiBlXMby'
+      domain: 'geotab-test.eu.auth0.com',
+      clientId: '9yco3W5jnTTNci40lFafK37UfrDkwXnn',
+      organization: 'org_dBKmIuK1mauVQAwV' // Geotab: org_JPhijibfOm0RSf1K  AT&T: org_dBKmIuK1mauVQAwV
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
